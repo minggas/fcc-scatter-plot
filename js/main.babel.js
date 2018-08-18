2022dba76faefc34ef798d3157b5d6094562339f
minggas <fasfora@gmail.com>
2018-08-17 23:20:40 -0300

    fix babel config

--- js/main.babel.js
+++ js/main.babel.js
@@ -1,15 +1,17 @@
-const end = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
+"use strict";
 
-const margin = {
+var end = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
+
+var margin = {
   top: 80,
   right: 30,
   bottom: 40,
   left: 80
 };
-let w = 920 - margin.left - margin.right,
+var w = 920 - margin.left - margin.right,
     h = 630 - margin.top - margin.bottom;
 //Color function
-const legendColor = d3.scaleOrdinal(d3.schemeSet1);
+var legendColor = d3.scaleOrdinal(d3.schemeSet1);
 //Place function
 String.prototype.ordinalNumber = function () {
   if (this[this.length - 1] === "1") {
@@ -24,26 +26,32 @@ String.prototype.ordinalNumber = function () {
   return this + "th";
 };
 //Tooltip
-let tooltip = d3.select(".container").append("div").attr("id", "tooltip").style("opacity", 0);
+var tooltip = d3.select(".container").append("div").attr("id", "tooltip").style("opacity", 0);
 //Chart container
-let svgContainer = d3.select(".container").append("svg").attr("class", "chart").attr("width", w + margin.left + margin.right).attr("height", h + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svgContainer = d3.select(".container").append("svg").attr("class", "chart").attr("width", w + margin.left + margin.right).attr("height", h + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 //Fetch the data
-d3.json(end).then(data => {
-  data.forEach(d => {
+d3.json(end).then(function (data) {
+  data.forEach(function (d) {
     var parsedTime = d.Time.split(":");
     d.MinTime = d.Time;
     d.Time = new Date(Date.UTC(1970, 0, 1, 0, parsedTime[0], parsedTime[1]));
   });
 
   //Scale the chart with the data
-  let xScale = d3.scaleLinear().domain([d3.min(data, d => d.Year - 1), d3.max(data, d => d.Year + 1)]).range([0, w]);
-  let yScale = d3.scaleTime().domain(d3.extent(data, d => d.Time)).range([0, h]);
+  var xScale = d3.scaleLinear().domain([d3.min(data, function (d) {
+    return d.Year - 1;
+  }), d3.max(data, function (d) {
+    return d.Year + 1;
+  })]).range([0, w]);
+  var yScale = d3.scaleTime().domain(d3.extent(data, function (d) {
+    return d.Time;
+  })).range([0, h]);
 
   //Make the Axis
-  const timeFormat = d3.timeFormat("%M:%S");
-  const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
-  const yAxis = d3.axisLeft(yScale).tickFormat(timeFormat);
+  var timeFormat = d3.timeFormat("%M:%S");
+  var xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
+  var yAxis = d3.axisLeft(yScale).tickFormat(timeFormat);
 
   //Put the X axis on the chart
   svgContainer.append("g").attr("class", "x axis").attr("id", "x-axis").attr("transform", "translate(0," + h + ")").call(xAxis).append("text").attr("class", "x-axis-label").attr("x", w).attr("y", -6).style("text-anchor", "end").text("Year");
@@ -54,11 +62,23 @@ d3.json(end).then(data => {
   svgContainer.append("text").attr("class", "label").attr("transform", "rotate(-90)").attr("x", -150).attr("y", -44).style("font-size", 18).text("Time in Minutes");
 
   //Put the chart on the DOM
-  svgContainer.selectAll(".dot").data(data).enter().append("svg:a").attr("xlink:href", d => d.URL).attr("target", "_blank").append("circle").attr("class", "dot").attr("data-xvalue", (d, i) => parseInt(d.Year)).attr("data-yvalue", (d, i) => d.Time.toISOString()).attr("cx", d => xScale(d.Year)).attr("cy", d => yScale(d.Time)).attr("r", 5).style("fill", d => legendColor(d.Doping !== ""))
+  svgContainer.selectAll(".dot").data(data).enter().append("svg:a").attr("xlink:href", function (d) {
+    return d.URL;
+  }).attr("target", "_blank").append("circle").attr("class", "dot").attr("data-xvalue", function (d, i) {
+    return parseInt(d.Year);
+  }).attr("data-yvalue", function (d, i) {
+    return d.Time.toISOString();
+  }).attr("cx", function (d) {
+    return xScale(d.Year);
+  }).attr("cy", function (d) {
+    return yScale(d.Time);
+  }).attr("r", 5).style("fill", function (d) {
+    return legendColor(d.Doping !== "");
+  })
   //Make the tooltip show data with the mouse over
-  .on("mouseover", (d, i) => {
+  .on("mouseover", function (d, i) {
     tooltip.transition().duration(200).style("opacity", 0.8);
-    tooltip.html(`${d.Name}: ${d.Nationality} - ${d.Place.toString().ordinalNumber()} Place<br/>Year: ${d.Year} Time: ${d.MinTime}<br/><br/>${d.Doping}`).attr("data-year", d.Year).style("left", d3.event.pageX + 15 + "px").style("top", d3.event.pageY - 5 + "px");
+    tooltip.html(d.Name + ": " + d.Nationality + " - " + d.Place.toString().ordinalNumber() + " Place<br/>Year: " + d.Year + " Time: " + d.MinTime + "<br/><br/>" + d.Doping).attr("data-year", d.Year).style("left", d3.event.pageX + 15 + "px").style("top", d3.event.pageY - 5 + "px");
   }).on("mouseout", function (d) {
     tooltip.transition().duration(200).style("opacity", 0);
   });
@@ -68,17 +88,17 @@ d3.json(end).then(data => {
   svgContainer.append("text").attr("x", w / 2).attr("y", -margin.top / 2 + 25).attr("text-anchor", "middle").style("font-size", "1rem").text("35 Fastest Time up Alpe d'Huez");
 
   //Add the Legend to the Chart
-  let legend = svgContainer.selectAll("legend").data(legendColor.domain()).enter().append("g").attr("id", "legend").attr("transform", function (d, i) {
+  var legend = svgContainer.selectAll("legend").data(legendColor.domain()).enter().append("g").attr("id", "legend").attr("transform", function (d, i) {
     return "translate(0," + (h / 2 - i * 20) + ")";
   });
 
   legend.append("rect").attr("x", w - 18).attr("width", 18).attr("height", 18).style("fill", legendColor);
 
-  legend.append("text").attr("x", w - 24).attr("y", 9).attr("dy", ".35em").style("text-anchor", "end").text(d => {
+  legend.append("text").attr("x", w - 24).attr("y", 9).attr("dy", ".35em").style("text-anchor", "end").text(function (d) {
     if (d) return "Riders with doping allegations";else {
       return "No doping allegations";
     }
   });
-}).catch(err => {
+}).catch(function (err) {
   alert(err);
 });
